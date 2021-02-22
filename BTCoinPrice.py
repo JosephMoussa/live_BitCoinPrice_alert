1c7f7c06be57f0a40ca6f1a7315d19306d5f956b
Joseph Moussa <52611349+JosephMoussa@users.noreply.github.com>
2021-02-22 18:44:22 +0200

    Add files via upload

---
+++ BTCoinPrice.py
@@ -0,0 +1,16 @@
+# Get the current price of Bitcoin in USD using the coindesk.com API with Python and the Requests HTTP library
+
+import requests # http://docs.python-requests.org/en/master/
+import winsound
+
+btc = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+
+#print(btc.status_code)
+#print(btc.headers['content-type'])
+#print(btc.encoding)
+#print(btc.text)
+while 1<2:
+    import time
+    time.sleep(10)
+    print(btc.json()['bpi']['USD']['rate'])
+    winsound.PlaySound("meme", winsound.SND_FILENAME)
